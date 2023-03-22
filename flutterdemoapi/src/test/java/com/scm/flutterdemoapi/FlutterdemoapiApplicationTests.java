b0ae8737ba493e598539b0ebe91605a10f2bab34
Sai-Zaw-Myint <saizawmyint@webinabi.jp>
2023-03-22 16:29:52 +0630

    initial commit & project setup

---
+++ flutterdemoapi/src/test/java/com/scm/flutterdemoapi/FlutterdemoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.flutterdemoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterdemoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
